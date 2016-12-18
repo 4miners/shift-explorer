e186fe110c6e3392edf4639e79593de4c56ba6fb
4miners <mariusz@serek.net>
2016-12-18 20:54:28 +0100

    Changed LSK to SHIFT in some places

--- public/src/js/services/activityGraph.js
+++ public/src/js/services/activityGraph.js
@@ -229,7 +229,7 @@ ActivityGraph.prototype.addAccount = function (id) {
 };
 
 ActivityGraph.prototype.amount = function (tx, sign) {
-    return (sign + tx.amount / Math.pow(10, 8)) + ' LSK';
+    return (sign + tx.amount / Math.pow(10, 8)) + ' SHIFT';
 };
 
 ActivityGraph.prototype.addTxSender = function (tx) {
